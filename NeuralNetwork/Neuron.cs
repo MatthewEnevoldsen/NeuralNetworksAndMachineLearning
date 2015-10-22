a1fdeb562c0d970b3316141ad0e534445d2dfa08
Matthew Enevoldsen <mattene@hotmail.co.uk>
2015-10-22 11:27:21 +0100

    Some stuff to create github

---
+++ NeuralNetwork/Neuron.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class Neuron
+    {
+        object PropagationFunction;
+        object ActivationFunction;
+        object Threshold;
+    }
+}
