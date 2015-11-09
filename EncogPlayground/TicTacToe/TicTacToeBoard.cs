f88475467a7241c7de146d6e9ad92448dd6ab273
Matthew Enevoldsen <mattene@hotmail.co.uk>
2015-11-09 23:36:35 +0000

    TicTacToe

---
+++ EncogPlayground/TicTacToe/TicTacToeBoard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncogPlayground.TicTacToe
+{
+    public class TicTacToeBoard
+    {
+        private TicTacToeTileCollection _tiles;
+        private TicTacToePiece _nextPiece;
+
+        public TicTacToeBoard()
+        {
+            _tiles = new TicTacToeTileCollection();
+            _nextPiece = TicTacToePiece.X;
+        }
+
+        public IEnumerable<TicTacToeMove> GetValidMoves()
+        {
+            return _tiles.Cast<EmptyTicTacToeTile>().Select(t=>new TicTacToeMove(t, _nextPiece));
+        }
+
+        public IEnumerable<TicTacToeTile> GetCurrnetState()
+        {
+            return _tiles;
+        }
+        
+        public void PerformMove(TicTacToeMove move)
+        {
+            _tiles[move.Tile.Pos] = move.PerformMove();
+        }
+    }
+
+    public class TicTacToeMove
+    {
+        public TicTacToePiece Piece { get; private set; }
+        public EmptyTicTacToeTile Tile { get; private set; }
+
+        public TicTacToeMove(EmptyTicTacToeTile tile, TicTacToePiece piece)
+        {
+            Tile = tile;
+            Piece = piece;
+        }
+
+        public FilledTicTacToeTile PerformMove()
+        {
+            return Tile.PlacePiece(Piece);
+        }
+    }
+}
---
+++ EncogPlayground/TicTacToe/TicTacToeGameTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Engine.Network.Activation;
+
+namespace EncogPlayground.TicTacToe
+{
+    [TestClass]
+    public class TicTacToeGameTests
+    {
+
+
+        [TestMethod]
+        public void InitialBoardIsEmpty()
+        {
+            var board = new TicTacToeBoard();
+        }
+
+        [TestMethod]
+        public void FirstPieceIsX()
+        {
+
+        }
+
+        [TestMethod]
+        public void FirstTurnCanBeAnywhere()
+        {
+
+        }
+
+        [TestMethod]
+        public void CannotGoInSamePlaceTwice()
+        {
+
+        }
+
+        [TestMethod]
+        public void CannotPlace2XsAtOnce()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void CannotPlace2OsAtOnce()
+        {
+
+        }
+    }
+}
