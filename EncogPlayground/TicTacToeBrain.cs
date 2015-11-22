bef1a98620b54988f78d40ee21438c98d8d419d3
Matthew Enevoldsen <mattene@hotmail.co.uk>
2015-11-22 20:13:03 +0000

    How many neurons is good?

--- EncogPlayground/TicTacToeBrain.cs
+++ EncogPlayground/TicTacToeBrain.cs
@@ -11,6 +11,8 @@ using Encog.Neural.Networks.Training.Propagation.Back;
 using Encog.ML.Data.Basic;
 using Encog.ML.Data;
 using System.Drawing;
+using System.Threading;
+using System.IO;
 
 namespace EncogPlayground
 {
@@ -40,41 +42,63 @@ namespace EncogPlayground
 
         public void CanItLearnTheRules()
         {
-            var nn = new BasicNetwork();
-            nn.AddLayer(new BasicLayer(new ActivationSigmoid(), false, 9));//input the grid contents
-            nn.AddLayer(new BasicLayer(new ActivationSigmoid(), true, 20));
-            nn.AddLayer(new BasicLayer(new ActivationSigmoid(), true, 20));
-            nn.AddLayer(new BasicLayer(new ActivationSigmoid(), false, 2));//next square to put a piece (x,y)
-            nn.Structure.FinalizeStructure();
-            nn.Reset();
-
-            var p1 = new RecordingPlayer(new RandomPlayer("first", Piece.X, new ValidMoveGetter()));
-            var p2 = new RecordingPlayer(new FirstPlayer("second", Piece.O, new ValidMoveGetter()));
-
-            new GameRunner().RunGames(p1, p2, 2000);
-
-            var trainingData = GetList(p2.AllMovesMade.Take(1900));
-            var verificationData = GetList(p2.AllMovesMade.Skip(1900).Take(100));
-
-            var dataSet = new BasicMLDataSet(trainingData.Cast<IMLDataPair>().ToList());
-
-            var train = new Backpropagation(nn, dataSet, 0.7, 0.3);
-            train.BatchSize = 1;
-            int epoch = 1;
-            do
+            var p = Parallel.For(1, 15, i =>
             {
-                train.Iteration();
-                epoch++;
-                if (epoch % 200 == 0)
-                    Console.WriteLine(train.Error);
-            } while (train.Error > 0.01);
+                var nn = new BasicNetwork();
+                nn.AddLayer(new BasicLayer(new ActivationTANH(), false, 9));//input the grid contents
+                for (int j = 0; j < i; j++)
+                {
+                    nn.AddLayer(new BasicLayer(new ActivationTANH(), true, i));
+                }
+                nn.AddLayer(new BasicLayer(new ActivationTANH(), false, 9));//next square to put a piece (x,y)
+                nn.Structure.FinalizeStructure();
+                nn.Reset();
 
-            foreach (var verf in verificationData)
-            {
-                var output = nn.Compute(verf.Input);
-                Console.WriteLine("Output: [{0}, {1}] for expected {2} ", Math.Round(output[0]), Math.Round(output[1]), verf.Ideal);
-            }
+                var p1 = new RecordingPlayer(new RandomPlayer("first", Piece.X, new ValidMoveGetter()));
+                var p2 = new RecordingPlayer(new FirstPlayer("second", Piece.O, new ValidMoveGetter()));
+
+                new GameRunner().RunGames(p1, p2, 1000);
+
+                var trainingData = GetList(p2.AllMovesMade.Take(900));
+                var verificationData = GetList(p2.AllMovesMade.Skip(900).Take(100));
+
+                var dataSet = new BasicMLDataSet(trainingData);
+
+                var train = new Backpropagation(nn, dataSet, 0.7, 0.3);
+                train.BatchSize = 1;
+                int epoch = 1;
+                var sb = new StringBuilder();
+                do
+                {
+                    train.Iteration();
+                    epoch++;
+                    if (epoch % 1000 == 0)
+                        sb.AppendLine("Error: " + train.Error);
+                } while (train.Error > 0.01 && epoch < 100000);
+
+                foreach (var verf in verificationData)
+                {
+                    var output = nn.Compute(verf.Input);
+                    int good = 0;
+                    int bad = 0;
+                    if (Math.Round(output[0]) == Math.Round(verf.Ideal[0]) && Math.Round(output[1]) == Math.Round(verf.Ideal[1]))
+                    {
+                        good++;
+                    }
+                    else
+                    {
+                        bad++;
+                    }
+                    sb.AppendLine(string.Format("Good {0} bad {1} ", good, bad));
+                }
+                var path = @"C:\Users\matte\Desktop\Results\" + i + ".txt";
+                File.WriteAllText(path, sb.ToString());
+            });
+
+            while (!p.IsCompleted)
+                Thread.Sleep(10000);
             Console.Read();
+
         }
 
         private int IndexOfMove(IMLData data)
@@ -95,8 +119,8 @@ namespace EncogPlayground
         private IList<IMLDataPair> GetList(IEnumerable<Tuple<int[,], Point>> input)
         {
             return input.Select(t => new BasicMLDataPair(
-                new BasicMLData(t.Item1.Cast<int>().Select(i=> (double)i).ToArray()),
-                new BasicMLData(new double[] { t.Item2.X, t.Item2.Y }))).Cast<IMLDataPair>().ToList();
+                new BasicMLData(t.Item1.Cast<int>().Select(i => (double)i).ToArray()),
+                new BasicMLData(Enumerable.Range(0, 9).Select(i => i == t.Item2.X * 3 + t.Item2.Y ? 1.0 : 0.0).ToArray()))).Cast<IMLDataPair>().ToList();
         }
 
     }
@@ -133,11 +157,11 @@ namespace EncogPlayground
             int[,] gridState = new int[grid.Size, grid.Size];
             for (int y = 0; y < grid.Size; y++)
                 for (int x = 0; x < grid.Size; x++)
-                    gridState[x,y] = grid[x, y].Content.GetHashCode();
+                    gridState[x, y] = grid[x, y].Content.GetHashCode();
             AllMovesMade.Add(Tuple.Create(gridState, move.Pos));
             return move;
         }
 
-        
+
     }
 }
